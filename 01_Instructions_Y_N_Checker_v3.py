3c1b592419ce96626ad6f9386d3b7152f2f7959b
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-03 11:38:04 +1200

    Create 01_Instructions_Y_N_Checker_v3.py

---
+++ 01_Instructions_Y_N_Checker_v3.py
@@ -0,0 +1,22 @@
+"""Program to quiz and teach the first ten maori numerals - v3
+the instructions will display correctly, & no errors will be present
+Created by Elme Pieterse
+03/05/2023
+"""
+
+view_instructions = input("Do you want to see the instructions? "
+                          "(Yes/No): ").lower()
+instructions = "Maori Numerals Quiz Instructions:\n"
+
+if view_instructions == "yes":
+    print(instructions)
+elif view_instructions == "y":
+    print(instructions)
+elif view_instructions == "no":
+    print("Program continues")
+elif view_instructions == "n":
+    print("Program continues")
+else:
+    print("Please enter either 'Yes' or 'No': ")
+
+print(f"You entered: {view_instructions}")
