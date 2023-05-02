937d6e7f00b8f52e55cb17ae29b7bb073bf415fe
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-03 11:45:16 +1200

    Update 01_Instructions_Y_N_Checker_v3.py

--- 01_Instructions_Y_N_Checker_v3.py
+++ 01_Instructions_Y_N_Checker_v3.py
@@ -6,7 +6,10 @@ Created by Elme Pieterse
 
 view_instructions = input("Do you want to see the instructions? "
                           "(Yes/No): ").lower()
-instructions = "Maori Numerals Quiz Instructions:\n"
+instructions = "Maori Numerals Quiz Instructions:\nThere are to difficulty " \
+               "levels, Easy(1) or Hard(2)\nYou will be quizzed based off " \
+               "your difficulty\nDifficulty: Easy\nYou will be given the " \
+               "maori name of a number\nYou will be asked what "
 
 if view_instructions == "yes":
     print(instructions)
