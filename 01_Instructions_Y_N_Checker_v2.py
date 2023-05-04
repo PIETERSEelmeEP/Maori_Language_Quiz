a798ef401de38a2a7233780cb2febe9f3e0d0e9c
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-04 12:12:38 +1200

    Update 01_Instructions_Y_N_Checker_v2.py

--- 01_Instructions_Y_N_Checker_v2.py
+++ 01_Instructions_Y_N_Checker_v2.py
@@ -1,22 +1,27 @@
-"""Program to quiz and teach the first ten maori numerals - v2
+"""Program's instructions & Yes/No Checker - v2
 y/n checker will be improved so that the code not only accepts 'yes' & 'no',
 but also other variations of answers e,g. 'y' & 'n'
+As well as simplifying the input to lower cases
 Created by Elme Pieterse
 03/05/2023
 """
 
+# Ask if user wants to view instructions
 view_instructions = input("Do you want to see the instructions? "
-                          "(Yes/No): ")
+                          "(Yes/No): ").lower()
+
 instructions = "Maori Numerals Quiz Instructions"
 
-if view_instructions == "yes":
+# If user input 'yes' or 'y' output will be the instructions
+if view_instructions == "yes" or view_instructions == "y":
     print(instructions)
-elif view_instructions == "y":
-    print(instructions)
-elif view_instructions == "no":
+
+# If user input 'no' or 'n', 'Program continues' will be the output
+elif view_instructions == "no" or view_instructions == "n":
     print("Program continues")
-elif view_instructions == "n":
-    print("Program continues")
+
+# Otherwise the required inputs will be displayed
 else:
     print("Please enter either 'Yes' or 'No': ")
 
+print(f"You entered: {view_instructions}")
