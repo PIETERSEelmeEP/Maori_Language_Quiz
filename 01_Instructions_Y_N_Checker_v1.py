1198effe179638e71fee624ad999db1f4f4b6281
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-04 12:05:53 +1200

    Update 01_Instructions_Y_N_Checker_v1.py

--- 01_Instructions_Y_N_Checker_v1.py
+++ 01_Instructions_Y_N_Checker_v1.py
@@ -1,17 +1,22 @@
-"""Program to quiz and teach the first ten maori numerals - v1
+"""Program's instructions & Yes/No Checker - v1
 ask if user wants to view instructions & yes/no checker
 Created by Elme Pieterse
 03/05/2023
 """
 # Ask if user wants to view instructions
 view_instructions = input("Do you want to see the instructions? (Yes/No): ")
+
 instructions = "Maori Numerals Quiz Instructions"
 
+# If user inputs 'yes' output the instructions
 if view_instructions == "yes":
     print(instructions)
+
+# If user inputs 'no' output 'Program continues'
 elif view_instructions == "no":
     print("Program continues")
+
+# Otherwise display required inputs
 else:
     print("Please enter either 'Yes' or 'No': ")
 
-print(f"You entered: {view_instructions}")
