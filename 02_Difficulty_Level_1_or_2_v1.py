e4ec9d4c2583f627bf5a1618f079c5dde1c1e9da
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-04 18:50:15 +1200

    Update 02_Difficulty_Level_1_or_2_v1.py

---
+++ 02_Difficulty_Level_1_or_2_v1.py
@@ -0,0 +1,20 @@
+"""Program asking difficulty Level - v1
+ask what level of difficulty does the user want to play, Easy(1) or Hard(2)
+Created by Elme Pieterse
+04/05/2023
+"""
+# Ask what level difficulty
+difficulty = int(input("What level of difficulty do you want to choose:\n"
+                       "If easy type '1', if hard type '2': "))
+
+# If user inputs '1' output the easy level questions
+if difficulty == 1:
+    print("Easy level questions")
+
+# If user inputs '2' output the hard level questions
+elif difficulty == 2:
+    print("Hard level questions")
+
+# Otherwise display required inputs
+else:
+    print("Please enter the numerals '1' or '2': ")
