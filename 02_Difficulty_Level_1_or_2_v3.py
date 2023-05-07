706381b5efe2e86fd208b502506fad73853316de
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-07 20:55:42 +1200

    Update 02_Difficulty_Level_1_or_2_v3.py

--- 02_Difficulty_Level_1_or_2_v3.py
+++ 02_Difficulty_Level_1_or_2_v3.py
@@ -1,39 +1,22 @@
 """Program asking difficulty Level - v3
-This program puts 02_Difficulty_Level_1_or_2_v2 into a function
+This includes valid numerals/difficulties as the basis of the while loop,
+resulting in the 'valid' variable being eliminated, creating a more efficient
+program
 Created by Elme Pieterse
 05/05/2023
 """
 
-
-# Functions:
-def difficulty_level_checker(question):
-    while True:
-
-        # Ask what level difficulty
-        difficulty_chosen = input(question)
-
-        # If user inputs '1' output the easy level questions
-        if difficulty_chosen == 1:
-            difficulty_chosen = 1
-            return difficulty_chosen
-
-        # If user inputs '2' output the hard level questions
-        elif difficulty_chosen == 2:
-            difficulty_chosen = 2
-            return difficulty_chosen
-
-        # Otherwise display required inputs
-        else:
-            print("Please enter the numerals '1' or '2'")
-
-
 # Main Routine
-difficulty_level = difficulty_level_checker("What level of difficulty do you "
-                                            "want to choose: Easy(1) or "
-                                            "Hard(2): ")
+error = "Please retry: Pick a difficulty, Easy(1) or Hard(2)"
+difficulty = 0
 
-if difficulty_level == 1:
-    print("Easy level questions")
-else:
-    print("Hard level questions")
+# Continuously asking until a valid input is given
+while not 1 <= difficulty <= 2:
+    try:
+        difficulty = int(input("What level of difficulty do you want to "
+                               "choose: Easy(1) or Hard(2): "))
 
+    except ValueError:
+        print(error)
+
+print(f"You entered the level {difficulty} difficulty")
