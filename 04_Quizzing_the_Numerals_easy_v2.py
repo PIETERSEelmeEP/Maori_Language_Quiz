de9837e398551217385cd1c0c217fc60d5502bc8
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-10 11:07:32 +1200

    Create 04_Quizzing_the_Numerals_easy_v2.py

---
+++ 04_Quizzing_the_Numerals_easy_v2.py
@@ -0,0 +1,8 @@
+"""Program quizzing the user of Maori Numerals (True or False quiz) - v2
+This will generate a random maori numeral and a random number
+between one & ten, all in a random order
+Created by Elme Pieterse
+10/05/2023
+"""
+
+
