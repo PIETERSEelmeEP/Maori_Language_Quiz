4c93f68318b158bafdf5b69f62bedf6144c67f55
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-15 20:07:37 +1200

    Update 05_Quizzing_the_Numerals_hard_v2.py

--- 05_Quizzing_the_Numerals_hard_v2.py
+++ 05_Quizzing_the_Numerals_hard_v2.py
@@ -6,16 +6,37 @@ Created by Elme Pieterse
 """
 
 
-def question_generator_hard():
+def question_generator_and_checker_hard():
     import random
-
     MAORI_NUMERALS = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu",
                       "waru", "iwa", "tekau"]
-
     maori_numeral = random.choice(MAORI_NUMERALS)
     question = input(f"What number is {maori_numeral}? ")
-    return question
+
+    def answer_checker(question_text):
+        while True:
+
+            # Input a question
+            answer = input(question_text).lower()
+
+            # If user input 'true' or 't' output will be 'checking if answer
+            if answer == "true" or answer == "t":
+                answer = "True"
+                return answer
+
+            # If user input 'false' or 'f' output will be 'checking if answer
+            elif answer == "false" or answer == "f":
+                answer = "False"
+                return answer
+
+            # Otherwise the required inputs will be displayed
+            else:
+                print("Please enter either 'True' or 'False': ")
+
+    answer_given = answer_checker(question).lower()
+    print("Checking if answer is correct.......")
+    return answer_given
 
 
 # Main Routine
-question_generator_hard()
+question_generator_and_checker_hard()
