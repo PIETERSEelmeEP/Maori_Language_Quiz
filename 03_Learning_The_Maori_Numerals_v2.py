07d11f2e988ce4ec3c62b5207a7d0be4cc6b7f1a
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-17 19:37:41 +1200

    Update 03_Learning_The_Maori_Numerals_v2.py

--- 03_Learning_The_Maori_Numerals_v2.py
+++ 03_Learning_The_Maori_Numerals_v2.py
@@ -1,27 +1,58 @@
-"""Program teaching the maori numerals 1 to 10 - v2
-learn/review maori numerals will be improved so that the code not only
-accepts 'yes' & 'no', but also other variations of answers e,g. 'y' & 'n'
-As well as simplifying the input to lower cases
+"""Program teaching the maori numerals 1 to 10 - v1
+this puts Maori numerals into a function with the correct results with the
+formatter
 Created by Elme Pieterse
 09/05/2023
 """
 
-# Ask if user wants to learn/review the maori numerals
-review_maori_numerals = input("Do you want to learn or revise the maori "
-                              "numerals? (Yes/No): ").lower()
 
-MAORI_NUMERALS = "Maori Numerals"
+def formatter(_symbol, _text):
+    sides = _symbol * 3
+    formatted_text = f"{sides} {_text} {sides}"
+    top_bottom = _symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
 
-# If user input 'yes' or 'y' output will be the maori numerals
-if review_maori_numerals == "yes" or review_maori_numerals == "y":
-    print(MAORI_NUMERALS)
 
-# If user input 'no' or 'n', 'Program continues' will be the output
-elif review_maori_numerals == "no" or review_maori_numerals == "n":
-    print("Program continues")
+def maori_numerals():
+    print(formatter("~", "Maori Numerals:"))
+    print()
+    print("Tahi = 1")
+    print("Rua = 2")
+    print("Toru = 3")
+    print("Wha = 4")
+    print("Rima = 5")
+    print("Ono = 6")
+    print("Whitu = 7")
+    print("Waru = 8")
+    print("Iwa = 9")
+    print("Tekau = 10")
+    print()
 
-# Otherwise the required inputs will be displayed
+
+def yes_no_checker(question_text):
+    while True:
+
+        # Ask if user wants to view instructions
+        answer = input(question_text).lower()
+
+        # If user input 'yes' or 'y' output will be the instructions
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If user input 'no' or 'n', 'Program continues' will be the output
+        elif answer == "no" or answer == 'n':
+            answer = "No"
+            return answer
+
+        # Otherwise the required inputs will be displayed
+        else:
+            print("Please enter either 'Yes' or 'No': ")
+
+
+review_maori_numerals = yes_no_checker("Do you want to learn or revise the "
+                                       "maori numerals? (Yes/No): ").lower()
+if review_maori_numerals == "yes":
+    maori_numerals()
 else:
-    print("Please enter either 'Yes' or 'No': ")
-
-print(f"You entered: {review_maori_numerals}")
+    print("Program continues")
