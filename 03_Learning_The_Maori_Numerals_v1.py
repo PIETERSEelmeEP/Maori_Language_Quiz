bb6017eb5d424bb6e8c0955c56eff7e7cb7ec2dd
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-17 19:37:38 +1200

    Update 03_Learning_The_Maori_Numerals_v1.py

--- 03_Learning_The_Maori_Numerals_v1.py
+++ 03_Learning_The_Maori_Numerals_v1.py
@@ -3,21 +3,33 @@ ask if user wants to learn/review the maori numerals 1 to 10
 Created by Elme Pieterse
 09/05/2023
 """
-# Ask if user wants to learn/review the maori numerals
-review_maori_numerals = input("Do you want to learn or revise the maori "
-                              "numerals? (Yes/No): ")
-
 MAORI_NUMERALS = "Maori Numerals"
 
-# If user inputs 'yes' output the maori numerals
+
+def yes_no_checker(question_text):
+    while True:
+
+        # Ask if user wants to view instructions
+        answer = input(question_text).lower()
+
+        # If user input 'yes' or 'y' output will be the instructions
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If user input 'no' or 'n', 'Program continues' will be the output
+        elif answer == "no" or answer == 'n':
+            answer = "No"
+            return answer
+
+        # Otherwise the required inputs will be displayed
+        else:
+            print("Please enter either 'Yes' or 'No': ")
+
+
+review_maori_numerals = yes_no_checker("Do you want to learn or revise the "
+                                       "maori numerals? (Yes/No): ")
 if review_maori_numerals == "yes":
     print(MAORI_NUMERALS)
-
-# If user inputs 'no' output 'Program continues'
-elif review_maori_numerals == "no":
-    print("Program continues")
-
-# Otherwise display required inputs
 else:
-    print("Please enter either 'Yes' or 'No': ")
-
+    print("Program continues")
