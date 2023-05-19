dc01d6c71aff5558bd87195cf1f731b41c5e0bef
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-19 15:06:04 +1200

    Update 06_Play_again_v1.py

--- 06_Play_again_v1.py
+++ 06_Play_again_v1.py
@@ -1,23 +1,43 @@
 """Program's play again function - v1
-ask if user wants end the quiz of repeat
+ask if user wants end the quiz of repeat, all ready into functions
+I took the template from the instructions program, due to the same structure
 Created by Elme Pieterse
 19/05/2023
 """
-# Ask if user wants to end or restart the program
-end_program = input("Do you want to exit the quiz? (Yes/No: ")
-thanks_note = "thank you"
 
-# If user inputs 'yes' output the thanks note
-if end_program == "yes":
-    print(thanks_note)
 
-# If user inputs 'no' output 'Program restarts'
-elif end_program == "no":
-    print("Program restarts")
+# Functions:
+# yes/no checker function:
+def yes_no_checker(question_text):
+    while True:
 
-# Otherwise display required inputs
+        # Ask if user wants to view instructions
+        answer = input(question_text).lower()
+
+        # If user input 'yes' or 'y' output will be the instructions
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If user input 'no' or 'n', 'Program continues' will be the output
+        elif answer == "no" or answer == 'n':
+            answer = "No"
+            return answer
+
+        # Otherwise the required inputs will be displayed
+        else:
+            print("Please enter either 'Yes' or 'No': ")
+
+
+# thank you function:
+def thanks_note():
+    print("Thank you")
+
+
+# Main Routine:
+exit_program = yes_no_checker("Do you want to exit the quiz? "
+                              "(Yes/No): ").lower()
+if exit_program == 'yes':
+    thanks_note()
 else:
-    print("Please enter either 'Yes' or 'No': ")
-
-
-
+    print("Program restarts")
