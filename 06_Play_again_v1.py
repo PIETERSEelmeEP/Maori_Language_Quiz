e31d19e071d522bd25ec5bdd3eca1e4cdadd53bf
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-05-19 14:58:46 +1200

    Update 06_Play_again_v1.py

--- 06_Play_again_v1.py
+++ 06_Play_again_v1.py
@@ -3,6 +3,21 @@ ask if user wants end the quiz of repeat
 Created by Elme Pieterse
 19/05/2023
 """
-# Ask if user wants to view instructions
+# Ask if user wants to end or restart the program
+end_program = input("Do you want to exit the quiz? (Yes/No: ")
+thanks_note = "thank you"
+
+# If user inputs 'yes' output the thanks note
+if end_program == "yes":
+    print(thanks_note)
+
+# If user inputs 'no' output 'Program restarts'
+elif end_program == "no":
+    print("Program restarts")
+
+# Otherwise display required inputs
+else:
+    print("Please enter either 'Yes' or 'No': ")
+
 
 
