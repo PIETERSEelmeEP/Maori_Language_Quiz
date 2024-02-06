da222b1ac5662261f4c51ce2dbb4d17ab9b43a68
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-06 16:09:53 +1300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("Hello, Welcome to the coding universe")
+name = input("What would you like us to call you?: ")
+print(f"We are pleased you could join us, {name}")
+age = int(input(f"What is your current age, {name}?: "))
+feedback = input("Why would you like to enter the coding universe?: ")
+print("Lets us begin!")
